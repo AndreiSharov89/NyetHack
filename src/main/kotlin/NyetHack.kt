59435f7c88fe6e05d85c8e5dd5db8e5e6db6a5c9
Andrei Sharov <sharov89@gmail.com>
2025-04-29 20:33:07 +0300

    chapter 8 lambdas

---
+++ src/main/kotlin/NyetHack.kt
@@ -0,0 +1,23 @@
+package org.example
+
+fun main() {
+    narrate("A hero enters the town of Kronshtadt. What is their name?", ::makeYellow)
+    val heroName = readLine()
+    require(heroName != null && heroName.isNotEmpty()) {
+        "A hero must have a name."
+    }
+
+    changeNarratorMood()
+    narrate("$heroName, ${createTitle(heroName)} heads to the town square.")
+}
+
+private fun makeYellow(message: String) = "\u001b[33;1m$message\u001b[0m"
+
+private fun createTitle(name: String): String {
+    return when {
+        name.all { it.isDigit() } -> "The Identifiable"
+        name.none { it.isLetter() } -> "The Witness Protection Member"
+        name.count { it.lowercase() in "aeiou" } > 4 -> "The Master of Vowel"
+        else -> "The Renowned Hero"
+    }
+}
